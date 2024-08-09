bd2987100d64632dd62a4c6da521cb67bc0a3037
mitvr006 <pawankumar3439744@gmail.com>
2024-08-09 13:18:06 +0530

    guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,21 +1,24 @@
 import random
+import logging
+logging.basicConfig(level=logging.INFO)
 
 """to pick a random number and store in guess"""
 guess=random.randint(1,100)
 
-print("welcome to the guess the number game\n")
+logging.info("\nwelcome to the guess the number game:\n")
+
 while True:
     user_guess=int(input("guess a number between 1 to 100:"))
     if user_guess==guess:
-        print("congratulation, you guess the right number")
+        logging.info("congratulation, you guess the right number")
         break
     elif (guess - 10) <= user_guess <= (guess + 10):
-        print("you are so close")
+        logging.info("you are so close")
     elif user_guess<guess:
-        print("your guess is to low")
+        logging.info("your guess is to low")
     elif user_guess>guess:
-        print("your guess is to high")
+        logging.info("your guess is to high")
     else:
-        print("enter a valid number between 1-100")
+        logging.info("enter a valid number between 1-100")
 
         
